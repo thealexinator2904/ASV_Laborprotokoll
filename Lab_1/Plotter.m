c43f3ea6201e2ff367cb9c38c38cfb34fdf942c6
Alexander Grebien <59167072+thealexinator2904@users.noreply.github.com>
2020-09-12 15:37:31 +0200

    improved plots. i hope

--- Lab_1/Plotter.m
+++ Lab_1/Plotter.m
@@ -17,7 +17,8 @@ struct_niinv_verst_meas = importdata('Messungen/niinv_verst_meas.txt');
 %Messungen
 
 %% Folgerschaltung
-fig_folger = figure('Name', 'Folgerschaltung', 'units','normalized','outerposition',[0 0 1 1]);
+fig_folger = figure('Name', 'Folgerschaltung');
+pos_fig1 = [0 0 800 800];
 %print sim
 bodePlot_din461(fig_folger,struct_folger_sim.data(:,1),...
     struct_folger_sim.data(:,4),...
@@ -29,12 +30,14 @@ bodePlot_din461(fig_folger, struct_folger_meas.data(:,1), ...
     struct_folger_meas.data(:,3),...
     struct_folger_meas.data(:,4),'*-', true);
 legend('Simulation', 'Messung');
+set(fig_folger,'Position',pos_fig1)
+
 subplot(2,1,1)
 %ylim([-20 1])
 
 
 %% invertierender Verstï¿½rker
-fig_inv_verst = figure('Name', 'invertierender Verstärker', 'units','normalized','outerposition',[0 0 1 1]);
+fig_inv_verst = figure('Name', 'invertierender Verstärker');
 % print sims 
 for i = 0 : 1
 bodePlot_din461(fig_inv_verst,struct_inv_verst_sim.data(:,1),...
@@ -53,10 +56,12 @@ subplot(2,1,1)
 %xlim([1 1e5])
 ylim([-10 61])
 legend('simualtion \nu = 60dB', 'simualtion \nu = 40dB', 'Messung \nu=20dB')
+set(fig_inv_verst,'Position',pos_fig1)
+
 
 %% nicht invertierender Verstï¿½rker
 
-fig_niinv_verst = figure('Name', 'nicht invertierender VerstÃ¤rker', 'units','normalized','outerposition',[0 0 1 1]);
+fig_niinv_verst = figure('Name', 'nicht invertierender VerstÃ¤rker');
 % Print sims
 for i = 0 : 2
 bodePlot_din461(fig_niinv_verst,struct_niinv_verst_sim.data(:,1),...
@@ -76,6 +81,8 @@ bodePlot_din461(fig_niinv_verst, struct_niinv_verst_meas.data(:,1), ...
     struct_niinv_verst_meas.data(:,5), ...
     struct_niinv_verst_meas.data(:,6), '-*', true);
 legend('sim1', 'sim2','sim3', 'sim4')
+set(fig_niinv_verst,'Position',pos_fig1)
+
 
 %% Figure export
 hgexport(fig_folger,'Plots/Folger.eps')
